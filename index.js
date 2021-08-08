cf21a714b9d28798b2d6a7a2f7ea613727f1acd6
Alvaro <alvarocabrales@ingenieros.com>
2021-08-08 16:12:06 -0500

    se crea el crud implementando las rutas para la tabla de films que ya cuenta con su modelo y solo restaba implementar las peticiones

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express');
-//const bodyParser=require('body-parser');
+const apiRouter=require('./routes/api');
 
 const app= express();
 
 require('./db');
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended:true}));
-
-//por si el bodyParser falla al estar deprecated
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+/*
 app.get('/',(req, rest)=>{
     rest.send('Hola mundo');
 })
+*/
+
+app.use('/api',apiRouter);
 
 app.listen(3000, ()=>{
     console.log('servidor andando');
